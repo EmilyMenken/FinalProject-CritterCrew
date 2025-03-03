8d51e4131261dc4687bf7145c065b199a3fc53f0
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-03-03 13:33:29 -0800

    started implementing app.js, validation.js, and createAccount.ejs

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 import express from 'express';
 import mariadb from 'mariadb';
+import dotenv from 'dotenv';
+import { validateNewUser, validateNewAppointment} from './services/validation.js';
+
+dotenv.config();
+
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
+});
+
+const PORT = process.env.APP_PORT || 3000;
+const uid; // how do I get this from the server when someone logs in?
+
+async function connect() {
+    try {
+        const conn = await pool.getConnection();
+        console.log('Connected to the database!');
+        return conn;
+    } catch (err) {
+        console.log(`Error connecting to MariaDB: ${error}`);
+    }
+}
 
 const app = express();
-
-// const pool = mariadb.createPool({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'petbarber',
-//     port: '3306'
-// });
-
-// async function connect() {
-//     try {
-//         const conn = await pool.getConnection();
-//         console.log('Connected to the database!');
-//         return conn;
-//     } catch (err) {
-//         console.log(`Error connecting to MariaDB: ${error}`);
-//     }
-// }
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -27,12 +34,84 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
-const PORT = 3000;
-
+// go to the home page
 app.get('/', (req, res) => {
     res.render('home');
 });
 
+// go to the create account page
+app.get('/createAccount', (req, res) => {
+    res.render('createAccount');
+});
+
+// go to the log in page
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+});
+
+app.post('/newAccount', async (req, res) => {
+    const newAccount = {
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        password: req.body.password,
+        phone: req.body.phone,
+        street_address: req.body.street_address,
+        city: req.body.city,
+        state: req.body.state,
+        zip_code: req.body.zipcode
+    }
+
+    const result = validateNewUser(newAccount);
+    if (!result.isValid) {
+        console.log(result.errors);
+        res.send(result.errors);
+        return;
+    }
+
+    newAccount.phone = newAccount.phone.replace(/-/g, "");
+
+    const conn = await connect();
+
+    const insertQuery = await conn.query(`insert into users
+        (fname, lname, email, password, phone, street_address, city, state, zip_code)
+        values ('?', '?', '?', '?','?', '?', '?', '?', '?')`,
+        [ newAccount.fname, newAccount.lname, newAccount.email, newAccount.password, newAccount.phone, newAccount.street_address, newAccount.city, newAccount.state, newAccount.zip_code ]
+    );
+
+    res.render('accountSuccess', { newAccount });
+
+});
+
+
+// TODO: Implement create new appointment
+app.post('/newAppointment', async (req, res) => {
+    
+    
+    const newAppointment = {
+       uid: 
+    }
+
+    const result = validateNewAppointment(newAppointment);
+    if (!result.isValid) {
+        console.log(result.errors);
+        res.send(result.errors);
+        return;
+    }
+
+    const conn = await connect();
+
+    const insertQuery = await conn.query(`insert into appointment
+        (uid, appt_date, petname, pettype, service, friendly)
+        values ('?', '?', '?', '?', '?', '?')`,
+        [ newAppointment.uid, newAccount.lname, newAccount.email, newAccount.password, newAccount.phone, newAccount.street_address, newAccount.city, newAccount.state, newAccount.zip_code ]
+    );
+
+    res.render('accountSuccess', { newAccount });
+
+});
+
+
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
-})
+});
--- services/validation.js
+++ services/validation.js
@@ -1,5 +1,4 @@
-export function validateForm(data) {
-    
+export function validateNewUser(data) {
     // Store all the validation errors in an array
     const errors = [ ];
     
@@ -20,8 +19,29 @@ export function validateForm(data) {
         errors.push("Email is required and must be valid");
     }
     
+    // Validate password
+    // Must include capital, lowercase, 1 symbol
+
+    // Validate street_address
+    // must not be blank
+
+    // Validate city
+    // must not be blank
+
+    // Validate state
+    // 2 letters from list of states
+
+    // Validate zip_code
+    // 5 numbers
+
     return {
         isValid: errors.length === 0,
         errors
     }
+}
+
+export function validateNewAppointment(data) {
+    const errors = [];
+
+    
 }
