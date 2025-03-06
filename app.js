e8ad47892088624c36dbe6e507da539a4286c0e6
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-03-06 13:33:32 -0800

    partial validation and account creation

--- app.js
+++ app.js
@@ -15,8 +15,11 @@ const pool = mariadb.createPool({
 
 const PORT = process.env.APP_PORT || 3000;
 
+// TODO: follow up on this in class to see how to do this better
 let loggedIn = false;
-let userID = -1;
+
+// TODO: Store the user query data from login?
+let userData = null;
 
 async function connect() {
     try {
@@ -46,7 +49,7 @@ app.get('/createAccount', (req, res) => {
     res.render('createAccount');
 });
 
-//TODO: Finish implementing validation
+//+TODO: Finish implementing validation
 app.post('/createAccount', async (req, res) => {
     const newAccount = {
         fname: req.body.fname,
@@ -60,20 +63,22 @@ app.post('/createAccount', async (req, res) => {
         zip_code: req.body.zipcode
     }
 
-
-    //TODO: uncomment after implementing validation
-    // const result = validateNewUser(newAccount);
-    // if (!result.isValid) {
-    //     console.log(result.errors);
-    //     res.send(result.errors);
-    //     return;
-    // }
+    //+TODO: uncomment after implementing validation
+    //TODO: test new account validation
+    const result = validateNewUser(newAccount);
+    if (!result.isValid) {
+        console.log(result.errors);
+        res.send(result.errors);
+        return;
+    }
 
     newAccount.phone = newAccount.phone.replace(/-/g, "");
     
     // console.log(newAccount);
 
     const conn = await connect();
+
+    // TODO: new user cannot have email that already exists on server (needs query
 
     const insertQuery = await conn.query(`insert into users
         (fname, lname, email, password, phone, street_address, city, state, zip_code)
@@ -93,32 +98,85 @@ app.get('/login', (req, res) => {
 
 // TODO: Implement logging in (post) route
 app.post('/login', async (req, res) => {
-    //things
+    // query if the username and password are correct
+    const loginInfo = {
+        email: req.body.email.trim(),
+        password: req.body.password.trim()
+    }
+
+    const conn = await connect();
+
+    // TODO: Query the DB to see if we have an account where the email and password matches
+    const results = await conn.query('SELECT * FROM users WHERE email = ? AND password = ?', [loginInfo.email, loginInfo.password]);
+
+    // TODO: If we have a username & password match in the DB, store the user info from the query and set logged in to true
+    if (results.length() === 1) {
+        userData = results[0];
+        loggedIn = true;
+    }
+
+    // TODO: // load the correct user page (user or admin) based on uid (admin account should be uid: 1)
+    res.render({userData});
 });
 
 // TODO: Implement create new appointment
 // TODO: How do I get uid out of the server?
 app.post('/newAppointment', async (req, res) => {
-    const result = validateNewAppointment(newAppointment);
-    if (!result.isValid) {
-        console.log(result.errors);
-        res.send(result.errors);
-        return;
+    // NOTE: Database fields
+    // aid int(5) auto_increment primary key,
+    // uid int(5),
+    // appt_date datetime,
+    // petname varchar(255),
+    // pettype varchar(255),
+    // service varchar(255),
+    // friendly boolean default false,
+    // foreign key (uid) references users(uid)
+    
+    const newAppointment = {
+        uid: userData.uid,
+        appt_date: req.body.appt_date,
+        petname: req.body.petname,
+        service: req.body.service,
+        friendly: req.body.friendly
     }
+    
+    // TODO: convert date format for DB
 
+    //TODO: verify uid against DB
+    // Follow up on this in class since it should really be done as part of validateNewAppointment
+
+    // TODO: uncomment after completing validation
+    // validate new appointment
+    // const result = validateNewAppointment(newAppointment);
+    // if (!result.isValid) {
+    //     console.log(result.errors);
+    //     res.send(result.errors);
+    //     return;
+    // }
+
+    // connect to the database
     const conn = await connect();
 
-    const insertQuery = await conn.query(`insert into appointment
-        (uid, appt_date, petname, pettype, service, friendly)
-        values (?, ?, ?, ?, ?, ?)`,
-        [ newAppointment.uid, newAccount.lname, newAccount.email, newAccount.password, newAccount.phone, newAccount.street_address, newAccount.city, newAccount.state, newAccount.zip_code ]
-    );
+    // TODO: implement add new appointment query
+    // const insertQuery = await conn.query(`insert into appointment
+    //     (uid, appt_date, petname, pettype, service, friendly)
+    //     values (?, ?, ?, ?, ?, ?)`,
+    //     [  ]
+    // );
 
+    // TODO: Create the account creation confirmation page
     res.render('accountSuccess', { newAccount });
 
 });
 
+// TODO: Implement logout
+app.get('/logout', (req, res) => {
 
+    loggedIn = false;
+    // TODO: set user account to empty
+    
+    res.render('home');
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
