1e1c7e789e6634ee0a34b3c52332965971e60761
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-03-18 16:10:52 -0700

    fixed app.js route and added more data to the default database script

--- pets-script.sql
+++ pets-script.sql
@@ -29,6 +29,10 @@ create table users(
 insert into users (fname, lname, email, password, phone, street_address, city, state, zip_code)
 values ('test', 'admin', 'admin@petbarber.com', 'password','1234567890', '123 Main St.', 'Auburn', 'WA', '98002');
 
+-- default test user
+insert into users (fname, lname, email, password, phone, street_address, city, state, zip_code)
+values ('test', 'user', 'user@gmail.com', 'wordpass','1234567890', '321 Main St.', 'Auburn', 'WA', '98002');
+
 -- create appointments
 create table appointment(
     aid int(5) auto_increment primary key,
@@ -41,7 +45,9 @@ create table appointment(
     foreign key (uid) references users(uid)
 );
 
--- test appointment
+-- test appointments
 insert into appointment (uid, appt_date, petname, pettype, service, friendly)
 values ('1', '2000-11-11 11:11:11', 'Spot', 'cat', 'bath', '1');
+insert into appointment (uid, appt_date, petname, pettype, service, friendly)
+values ('2', '2000-11-11 11:11:11', 'Mittens', 'dog', 'bath', '1');
 
